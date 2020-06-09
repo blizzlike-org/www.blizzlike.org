4fa1be899d22013433efcc4a75fe7af65b112910
shagu <ericmauser@googlemail.com>
2020-06-09 20:11:43 +0200

    css: update realminfo class names

--- js/realmd.js
+++ js/realmd.js
@@ -16,51 +16,51 @@ function createRealm(data, name) {
   const frame = document.createElement('div')
 
   /* left pane */
-  const overview = document.createElement('div')
-  overview.className = "realminfo-overview"
+  const left = document.createElement('div')
+  left.className = "realminfo-left"
   const realmname = document.createElement('span')
   realmname.className = "realminfo-realmname"
-  overview.append(realmname)
+  left.append(realmname)
   const uptime = document.createElement('span')
   uptime.className = "realminfo-uptime"
-  overview.append(uptime)
+  left.append(uptime)
 
   /* center pane */
-  const factions = document.createElement('div')
-  factions.className = "realminfo-factions"
+  const center = document.createElement('div')
+  center.className = "realminfo-center"
   const mode = document.createElement('span')
   mode.className = "realminfo-mode"
-  factions.append(mode)
+  center.append(mode)
   const horde = document.createElement('span')
-  factions.append(horde)
+  center.append(horde)
   const hordeicon = document.createElement('img')
   hordeicon.src = "img/horde.png"
   hordeicon.style.verticalAlign = "middle"
-  factions.append(hordeicon)
+  center.append(hordeicon)
   const allianceicon = document.createElement('img')
   allianceicon.src = "img/alliance.png"
   allianceicon.style.verticalAlign = "middle"
-  factions.append(allianceicon)
+  center.append(allianceicon)
   const alliance = document.createElement('span')
-  factions.append(alliance)
+  center.append(alliance)
 
   /* right pane */
-  const stats = document.createElement('div')
-  stats.className = "realminfo-stats"
+  const right = document.createElement('div')
+  right.className = "realminfo-right"
   const online = document.createElement('span')
   online.className = "realminfo-online"
-  stats.append(online)
+  right.append(online)
   const characters = document.createElement('span')
   characters.className = "realminfo-characters"
-  stats.append(characters)
+  right.append(characters)
   const accounts = document.createElement('span')
   accounts.className = "realminfo-accounts"
-  stats.append(accounts)
+  right.append(accounts)
 
   /* build frame */
-  frame.append(overview)
-  frame.append(factions)
-  frame.append(stats)
+  frame.append(left)
+  frame.append(center)
+  frame.append(right)
 
   /* update all values once a second */
   window.setInterval(function() {
